2ab6ec6a92ca3745c4c82de92ffb1d43abff8125
Humayun Raza <humayunraza5@gmail.com>
2023-01-03 03:05:08 +0500

    Karigar - made main page v1.01

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 const path = require('path')
 app.use(express.static(__dirname + '/public'));
+
 mongoose.connect("mongodb://localhost:27017/KarigarDB",{useNewUrlParser: true});
 
 const AdminSchema = {
@@ -14,7 +15,27 @@ const AdminSchema = {
     pass: String
 }
 
+const orderSchema = {
+    firstName:String,
+    lastName:String,
+    email: String,
+    PhoneNo:Number,
+    location:String,
+    city:String,
+    state:String,
+    zip:Number,
+    mouse:String,
+    fault:String
+}
+
+const Order = mongoose.model("Order",orderSchema);
+
 const Admin = mongoose.model("Admin", AdminSchema);
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+
 
 app.get("/admin",function(req,res){
     if(loggin == true) {
@@ -24,6 +45,57 @@ app.get("/admin",function(req,res){
     }
 })
 
+app.post("/", function(req,res){
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+    const number = req.body.number;
+    const address = req.body.address;
+    const city = req.body.city;
+    let stateN = req.body.State;
+    const postalCode = req.body.zipCode;
+    const mouseMod = req.body.mouse;
+    const fault = req.body.description;
+    var state = "";
+    console.log(stateN);
+    switch(stateN){
+        case "1":
+            state = "Sindh"
+            break;
+        case "2":
+            state = "Punjab"
+            break;
+        case "3":
+            state = "Khyber Pakhtunkhwa"
+            break;
+        case "4":
+            state = "Azad Jammu & Kashmir"
+            break;
+        case "5":
+            state = "Balochistan"
+            break;    
+        default: state = "No state found";
+        }
+    const order = new Order({
+        firstName: fname,
+        lastName: lname,
+        email:email,
+        PhoneNo:number,
+        location:address,
+        city:city,
+        state:state,
+        zip:postalCode,
+        mouse:mouseMod,
+        fault:fault
+    })
+    order.save();
+    console.log(order);
+
+    // order.save();
+
+    res.redirect("/");
+
+})
 
 app.post("/admin", function(req,res){
     const username = req.body.username;
